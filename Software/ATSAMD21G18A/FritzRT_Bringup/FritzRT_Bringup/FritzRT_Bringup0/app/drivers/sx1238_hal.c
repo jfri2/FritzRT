5c86e46b7419287310aca17eae006f9d7e42162a
John Fritz <jfritz022@gmail.com>
2017-09-05 16:42:51 -0500

    Updated HAL

--- Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
+++ Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
@@ -10,6 +10,7 @@
 /******************************************************************************
 Includes
 ******************************************************************************/
+#include <stdbool.h>
 #include <atmel_start.h>
 #include "sx1238_hal.h"
 
@@ -31,94 +32,102 @@ Public Variable Definitions
 Private Variables
 ******************************************************************************/
 static struct io_descriptor *sx1238_ctrl_spi_io;
-uint8_t sx1238_spi_byte_rw_buffer[SX1238_SPI_BYTE_RW_BUFFER_SIZE] = {0};
-uint8_t sx1238_spi_rw_buffer[SX1238_SPI_RW_BUFFER_MAX_SIZE] = {0};
+uint8_t sx1238_hal_spi_byte_rw_buffer[SX1238_SPI_BYTE_RW_BUFFER_SIZE] = {0};
+uint8_t sx1238_hal_spi_rw_buffer[SX1238_SPI_RW_BUFFER_MAX_SIZE] = {0};
 
 /******************************************************************************
 Private Function Declarations
 ******************************************************************************/
-void sx1238_spi_stop(void);
-void sx1238_spi_start(void);
+void sx1238_hal_spi_stop(void);
+void sx1238_hal_spi_start(void);
 
 /******************************************************************************
 Public Function Definitions
 ******************************************************************************/
-void sx1238_write(uint8_t addr, uint8_t data)
+void sx1238_hal_write(uint8_t addr, uint8_t data)
 {
-    sx1238_spi_byte_rw_buffer[0] = addr;
-    sx1238_spi_byte_rw_buffer[1] = data;
+    sx1238_hal_spi_byte_rw_buffer[0] = addr;
+    sx1238_hal_spi_byte_rw_buffer[1] = data;
 
     struct spi_xfer temp_xfer = 
     {
-        .txbuf = sx1238_spi_byte_rw_buffer,
-        .rxbuf = sx1238_spi_byte_rw_buffer,
+        .txbuf = sx1238_hal_spi_byte_rw_buffer,
+        .rxbuf = sx1238_hal_spi_byte_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
-    sx1238_spi_start();    
+    sx1238_hal_spi_start();    
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_spi_stop();
+    sx1238_hal_spi_stop();
 }
 
-uint8_t sx1238_read(uint8_t addr)
+uint8_t sx1238_hal_read(uint8_t addr)
 {
-    sx1238_spi_byte_rw_buffer[0] = addr;
-    sx1238_spi_byte_rw_buffer[1] = 0x00;
+    sx1238_hal_spi_byte_rw_buffer[0] = addr;
+    sx1238_hal_spi_byte_rw_buffer[1] = 0x00;
 
     struct spi_xfer temp_xfer =
     {
-        .txbuf = sx1238_spi_byte_rw_buffer,
-        .rxbuf = sx1238_spi_byte_rw_buffer,
+        .txbuf = sx1238_hal_spi_byte_rw_buffer,
+        .rxbuf = sx1238_hal_spi_byte_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
-    sx1238_spi_start();
+    sx1238_hal_spi_start();
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_spi_stop();
+    sx1238_hal_spi_stop();
 
-    return (sx1238_spi_byte_rw_buffer[1]);
+    return (sx1238_hal_spi_byte_rw_buffer[1]);
 }
 
-void sx1238_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
+void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
 {
-    sx1238_spi_rw_buffer[0] = addr;
+    sx1238_hal_spi_rw_buffer[0] = addr;
 
     for (uint16_t i = 0; i < size; i++)
     {
-        sx1238_spi_rw_buffer[i + 1] = buffer[i];
+        sx1238_hal_spi_rw_buffer[i + 1] = buffer[i];
     }
 
     struct spi_xfer temp_xfer =
     {
-        .txbuf = sx1238_spi_rw_buffer,
-        .rxbuf = sx1238_spi_rw_buffer,
+        .txbuf = sx1238_hal_spi_rw_buffer,
+        .rxbuf = sx1238_hal_spi_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
-    sx1238_spi_start();
+    sx1238_hal_spi_start();
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_spi_stop();
+    sx1238_hal_spi_stop();
 }
 
-void sx1238_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
+void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
 {
-    sx1238_spi_rw_buffer[0] = addr;
+    sx1238_hal_spi_rw_buffer[0] = addr;
 
     for (uint16_t i = 0; i < size; i++)
     {
-        sx1238_spi_rw_buffer[i + 1] = buffer[i];
+        sx1238_hal_spi_rw_buffer[i + 1] = buffer[i];
     }
 
     struct spi_xfer temp_xfer =
     {
-        .txbuf = sx1238_spi_rw_buffer,
-        .rxbuf = sx1238_spi_rw_buffer,
+        .txbuf = sx1238_hal_spi_rw_buffer,
+        .rxbuf = sx1238_hal_spi_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
-    sx1238_spi_start();
+    sx1238_hal_spi_start();
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_spi_stop();
+    sx1238_hal_spi_stop();
+}
+
+void sx1238_hal_reset(void)
+{
+    gpio_set_pin_level(PIN_SX1238_RESET, false);
+    sx1238_hal_delay_ms(1);
+    gpio_set_pin_level(PIN_SX1238_RESET, true);
+    sx1238_hal_delay_ms(10);
 }
 
 /******************************************************************************
@@ -128,7 +137,7 @@ Private Function Definitions
 * @brief Starts SX1238 control SPI
 * @return void
 */
-void sx1238_spi_start(void)
+void sx1238_hal_spi_start(void)
 {
     gpio_set_pin_level(PIN_SPI_CTRL_CS, false);
     spi_m_sync_get_io_descriptor(&CTRL_SPI, &sx1238_ctrl_spi_io);
@@ -139,7 +148,7 @@ void sx1238_spi_start(void)
 * @brief Stops SX1238 control SPI
 * @return void
 */
-void sx1238_spi_stop(void)
+void sx1238_hal_spi_stop(void)
 {
     spi_m_sync_get_io_descriptor(&CTRL_SPI, &sx1238_ctrl_spi_io);
     spi_m_sync_disable(&CTRL_SPI);
--- Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.h
+++ Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.h
@@ -19,8 +19,6 @@ Includes
 /******************************************************************************
 Defines
 ******************************************************************************/
-#define SX1238_SPI_RW_BUFFER_MAX_SIZE   256
-#define SX1238_SPI_BYTE_RW_BUFFER_SIZE  2
 
 /******************************************************************************
 Public Data Types
@@ -33,9 +31,17 @@ Public Variable Declarations
 /******************************************************************************
 Public Function Declarations
 ******************************************************************************/
-void sx1238_write(uint8_t addr, uint8_t data);
-uint8_t sx1238_read(uint8_t addr);
-void sx1238_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
-void sx1238_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
+void sx1238_hal_write(uint8_t addr, uint8_t data);
+uint8_t sx1238_hal_read(uint8_t addr);
+void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
+void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
+
+/******************************************************************************
+Public Inline Functions
+******************************************************************************/
+inline void sx1238_hal_delay_ms(uint16_t ms)
+{
+    delay_ms(ms);
+}
 
 #endif // _SX1238_HAL_H
