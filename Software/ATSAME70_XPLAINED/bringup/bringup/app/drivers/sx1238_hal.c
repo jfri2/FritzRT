ba0352d967a4da14b54da28956817992837885bd
John Fritz <jfritz022@gmail.com>
2017-09-05 22:52:50 -0500

    Fixed weird merge error (hopefully)

--- Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
+++ Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
@@ -10,7 +10,6 @@
 /******************************************************************************
 Includes
 ******************************************************************************/
-#include <stdbool.h>
 #include <atmel_start.h>
 #include "sx1238_hal.h"
 
@@ -50,7 +49,6 @@ void sx1238_hal_spi_start(void);
 /******************************************************************************
 Public Function Definitions
 ******************************************************************************/
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
 /*!
 * @brief Writes a byte to the SX1238 using SX1238 control spi
 * @param[in] addr  Address on SX1238 to write to
@@ -60,38 +58,23 @@ Public Function Definitions
 void sx1238_hal_write(uint8_t addr, uint8_t data)
 {
     // Load address into spi buffer and set to write access
-    sx1238_spi_byte_rw_buffer[0] = (addr | WNR_WRITE_BM);
-    sx1238_spi_byte_rw_buffer[1] = data;
-=======
-void sx1238_hal_write(uint8_t addr, uint8_t data)
-{
-    sx1238_hal_spi_byte_rw_buffer[0] = addr;
+    sx1238_hal_spi_byte_rw_buffer[0] = (addr | WNR_WRITE_BM);
     sx1238_hal_spi_byte_rw_buffer[1] = data;
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
 
     // Create spi transfer struct
     struct spi_xfer temp_xfer = 
     {
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
-        .txbuf = sx1238_spi_byte_rw_buffer,
-        .rxbuf = sx1238_spi_byte_rw_buffer,
-        .size = 2
-    };
-
-    // Do spi transfer
-=======
         .txbuf = sx1238_hal_spi_byte_rw_buffer,
         .rxbuf = sx1238_hal_spi_byte_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
+    // Do spi transfer
     sx1238_hal_spi_start();    
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
     sx1238_hal_spi_stop();
 }
 
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
 /*!
 * @brief Reads a byte to the SX1238 using SX1238 control spi
 * @param[in] addr  Address on SX1238 to read from
@@ -99,40 +82,24 @@ void sx1238_hal_write(uint8_t addr, uint8_t data)
 */
 uint8_t sx1238_hal_read(uint8_t addr)
 {
-    // Load address into spi buffer and set to read access
-    sx1238_spi_byte_rw_buffer[0] = (addr & WNR_READ_BM);
-    sx1238_spi_byte_rw_buffer[1] = 0x00;
-=======
-uint8_t sx1238_hal_read(uint8_t addr)
-{
-    sx1238_hal_spi_byte_rw_buffer[0] = addr;
+    sx1238_hal_spi_byte_rw_buffer[0] = (addr & WNR_READ_BM);
     sx1238_hal_spi_byte_rw_buffer[1] = 0x00;
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
 
     // Create spi transfer struct
     struct spi_xfer temp_xfer =
     {
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
-        .txbuf = sx1238_spi_byte_rw_buffer,
-        .rxbuf = sx1238_spi_byte_rw_buffer,
-        .size = 2
-    };
-
-    // Do spi transfer
-=======
-        .txbuf = sx1238_hal_spi_byte_rw_buffer,
-        .rxbuf = sx1238_hal_spi_byte_rw_buffer,
+        .txbuf = sx1238_hal_spi_rw_buffer,
+        .rxbuf = sx1238_hal_spi_rw_buffer,
         .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
+    // Do spi transfer
     sx1238_hal_spi_start();
     spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
     sx1238_hal_spi_stop();
 
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
     // Return data read from SX1238
-    return (sx1238_spi_byte_rw_buffer[1]);
+    return (sx1238_hal_spi_rw_buffer[1]);
 }
 
 /*!
@@ -145,16 +112,8 @@ uint8_t sx1238_hal_read(uint8_t addr)
 void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
 {
     // Load address into spi buffer and set to write access
-    sx1238_spi_rw_buffer[0] = (addr | WNR_WRITE_BM);
-=======
-    return (sx1238_hal_spi_byte_rw_buffer[1]);
-}
-
-void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
-{
-    sx1238_hal_spi_rw_buffer[0] = addr;
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
-
+    sx1238_hal_spi_rw_buffer[0] = (addr | WNR_WRITE_BM);
+    
     // Load data from buffer passed to this function to spi buffer
     for (uint16_t i = 0; i < size; i++)
     {
@@ -164,9 +123,8 @@ void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
     // Create spi transfer struct
     struct spi_xfer temp_xfer =
     {
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
-        .txbuf = sx1238_spi_rw_buffer,
-        .rxbuf = sx1238_spi_rw_buffer,
+        .txbuf = sx1238_hal_spi_rw_buffer,
+        .rxbuf = sx1238_hal_spi_rw_buffer,
         .size = size + 1
     };
 
@@ -186,35 +144,14 @@ void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
 void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
 {
     // Load address into spi buffer and set to read access
-    sx1238_spi_rw_buffer[0] = (addr & WNR_READ_BM);
-=======
-        .txbuf = sx1238_hal_spi_rw_buffer,
-        .rxbuf = sx1238_hal_spi_rw_buffer,
-        .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
-    };
-
-    sx1238_hal_spi_start();
-    spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_hal_spi_stop();
-}
-
-void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
-{
-    sx1238_hal_spi_rw_buffer[0] = addr;
-
-    for (uint16_t i = 0; i < size; i++)
-    {
-        sx1238_hal_spi_rw_buffer[i + 1] = buffer[i];
-    }
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
+    sx1238_hal_spi_rw_buffer[0] = (addr & WNR_READ_BM);
 
     // Create spi transfer struct
     struct spi_xfer temp_xfer =
     {
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.c
-        .txbuf = sx1238_spi_rw_buffer,
-        .rxbuf = sx1238_spi_rw_buffer,
-        .size = size + 1
+        .txbuf = sx1238_hal_spi_rw_buffer,
+        .rxbuf = sx1238_hal_spi_rw_buffer,
+        .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
     };
 
     // Do spi transfer
@@ -225,26 +162,20 @@ void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size)
     // Move data from temp spi buffer to buffer passed to this function
     for (uint16_t i = 0; i < size; i++)
     {
-        buffer[i] = sx1238_spi_rw_buffer[i + 1];
+        buffer[i] = sx1238_hal_spi_rw_buffer[i + 1];
     }
-=======
-        .txbuf = sx1238_hal_spi_rw_buffer,
-        .rxbuf = sx1238_hal_spi_rw_buffer,
-        .size = SX1238_SPI_BYTE_RW_BUFFER_SIZE
-    };
-
-    sx1238_hal_spi_start();
-    spi_m_sync_transfer(&CTRL_SPI, &temp_xfer);
-    sx1238_hal_spi_stop();
 }
 
+/*!
+* @brief Resets SX1238
+* @return void
+*/
 void sx1238_hal_reset(void)
 {
     gpio_set_pin_level(PIN_SX1238_RESET, false);
     sx1238_hal_delay_ms(1);
     gpio_set_pin_level(PIN_SX1238_RESET, true);
     sx1238_hal_delay_ms(10);
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.c
 }
 
 /******************************************************************************
--- Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.h
+++ Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.h
@@ -18,8 +18,6 @@ Includes
 /******************************************************************************
 Defines
 ******************************************************************************/
-#define SX1238_SPI_RW_BUFFER_MAX_SIZE   256
-#define SX1238_SPI_BYTE_RW_BUFFER_SIZE  2
 
 /******************************************************************************
 Public Data Types
@@ -32,13 +30,11 @@ Public Variable Declarations
 /******************************************************************************
 Public Function Declarations
 ******************************************************************************/
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.h
 void sx1238_hal_write(uint8_t addr, uint8_t data);
 uint8_t sx1238_hal_read(uint8_t addr);
 void sx1238_hal_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
 void sx1238_hal_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
-<<<<<<< HEAD:Software/ATSAME70_XPLAINED/bringup/bringup/app/drivers/sx1238_hal.h
-=======
+void sx1238_hal_reset(void);
 
 /******************************************************************************
 Public Inline Functions
@@ -47,12 +43,5 @@ inline void sx1238_hal_delay_ms(uint16_t ms)
 {
     delay_ms(ms);
 }
->>>>>>> 5c86e46b7419287310aca17eae006f9d7e42162a:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.h
-=======
-void sx1238_write(uint8_t addr, uint8_t data);
-uint8_t sx1238_read(uint8_t addr);
-void sx1238_write_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
-void sx1238_read_buffer(uint8_t addr, uint8_t *buffer, uint16_t size);
->>>>>>> parent of 5c86e46... Updated HAL:Software/ATSAMD21G18A/FritzRT_Bringup/FritzRT_Bringup/FritzRT_Bringup0/app/drivers/sx1238_hal.h
 
 #endif // _SX1238_HAL_H
